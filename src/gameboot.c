b59400c7a8102a6539c7def2d611c416dfe1df6a
zestydevy <zestydevy@gmail.com>
2026-05-03 11:28:54 -0500

    too early to determine stack ranges, hack for now

--- src/gameboot.c
+++ src/gameboot.c
@@ -6,12 +6,7 @@
 
 #define THREAD_ID_IDLE 1
 #define THREAD_ID_MAIN 6
-
-typedef struct {
-    u8 unk0[0xC000];
-    u8 padC000[0x120];
-    u8 unkC120[0xC0000];
-} Unk;
+#define ARRAYCOUNT(a) (sizeof(a) / sizeof(a[0]))
 
 extern u32 D_80037D20[];
 extern s32 D_80037F58;
@@ -34,9 +29,13 @@ extern OSThread D_80081C10;
 extern OSScClient D_80081DC0;
 extern OSMesgQueue D_80081DC8;
 extern OSMesg D_80081DE0[32];
-extern Unk D_80038188;
-extern u8 D_800442A8[];
-extern u8 D_8004FF50[];
+
+// TODO: too early to determine the range of these
+extern u8 D_80038188[0x8003FF50 - 0x80038188];
+extern u8 D_8003FF50[0xC000 - ARRAYCOUNT(D_80038188)];
+extern u8 D_800442A8[0x8004FF50 - 0x800442A8];
+extern u8 D_8004FF50[0xC000 - ARRAYCOUNT(D_800442A8)];
+
 extern u32 D_800351E0;
 extern u32 D_80037D24;
 extern u32 D_80052720[];
@@ -123,22 +122,24 @@ void func_8000152C(void) {
     u8 *stackPtr1;
     s32 i;
 
-    stackPtr0 = D_80038188.unk0;
-    stackPtr1 = D_80038188.unkC120;
+    stackPtr0 = D_80038188;
+    stackPtr1 = D_800442A8;
     for (i = 0; i < 0xC000; ++i) {
         *stackPtr0++ = 0xCC;
         *stackPtr1++ = 0xCC;
     }
 
-    D_80038188.unk0[0xC000 - 4] = 'T';
-    D_80038188.unk0[0xC000 - 3] = 'A';
-    D_80038188.unk0[0xC000 - 2] = 'I';
-    D_80038188.unk0[0xC000 - 1] = 'L';
+    D_8003FF50[0x4234] = 'T';
+    D_8003FF50[0x4235] = 'A';
+    D_8003FF50[0x4236] = 'I';
+    D_8003FF50[0x4237] = 'L';
 
-    D_80038188.unkC120[0xC000 - 4] = 'T';
-    D_80038188.unkC120[0xC000 - 3] = 'A';
-    D_80038188.unkC120[0xC000 - 2] = 'I';
-    D_80038188.unkC120[0xC000 - 1] = 'L';
+    if (D_8004FF50);
+
+    D_8004FF50[0x354] = 'T';
+    D_8004FF50[0x355] = 'A';
+    D_8004FF50[0x356] = 'I';
+    D_8004FF50[0x357] = 'L';
 }
 
 void func_800015BC(void *arg) {
